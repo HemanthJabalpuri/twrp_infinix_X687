64f55f2b4ba8106dce6477bb8f8d4d58e064e480
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2021-07-12 23:15:23 +0530

    Fix decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,20 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
+TW_INCLUDE_CRYPTO := true
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
---
+++ README.md
@@ -0,0 +1,19 @@
+## TWRP for Infinix Zero 8 X687
+
+### Working
+- Decryption
+- MTP
+- fastbootd
+- USB OTG
+
+### Issues
+- loop mount errors with Android 11
+
+
+### Specifications
+- See https://m.gsmarena.com/infinix_zero_8-10401.php
+
+### Credits
+- @mastersenpai05 for initial tree
+- @kusti420 for infinite testing
+- @lopestom for **Blu G90 Pro** TWRP at [here](https://github.com/lopestom/device_TWRP-PBRP_BLU_G0370WW)
