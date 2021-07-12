029ccbc2176ee7e04cd6dad87fef599662f744b7
lopestorm <lopestom1@gmail.com>
2021-07-12 19:19:26 -0300

    Add files for TEST-3
    Now @HemanthJabalpuri fixed enc/dec.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,11 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
 # Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,3 +16,5 @@
 
 PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/omni_X687.mk
+
+COMMON_LUNCH_CHOICES := omni_X687-eng
--- omni_X687.mk
+++ omni_X687.mk
@@ -1,13 +1,11 @@
 #
 # Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,25 +14,28 @@
 # limitations under the License.
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := Infinix ZERO 8
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X687
 PRODUCT_NAME := omni_X687
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := X687
-PRODUCT_MANUFACTURER := infinix
+PRODUCT_MODEL := Infinix X687
+PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
 
-PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_BOARD := mt6785
 
-# Dynamic Partition
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
