6ad01d6549cf78a018499bc18eeb9b1787e91c1e
Hemanth <hemanthjabalpuri@gmail.com>
2021-07-28 12:42:48 +0530

    Use base instead of aosp_base

--- omni_X687.mk
+++ omni_X687.mk
@@ -17,8 +17,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
